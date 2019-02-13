54167d15ad95dcc3b9d793df56cfbc6adc4574e7
DarkoBesevic <47328583+DarkoBesevic@users.noreply.github.com>
2019-02-13 17:54:21 +0100

    Add files via upload

---
+++ specs.js
@@ -0,0 +1,34 @@
+/* 
+ocekujemo da ce funkcija kojoj prosedimo niz brojeva vratiti samo parne brojeve iz niza  
+*/
+
+describe("Testiranje filter funkcije", function () {
+    const niz = [56, 26, 0, 77, 39, 346, 85, 55, 234, 5651, 5687, 588];
+    it("treba da vrati samo parne brojeve", function (niz) {
+        for (let i = 0; i < niz.length; i++) {
+            if (niz[i] % 2 == 1) {
+                let parni = false;
+                break;
+            }
+        }
+        expect(parni).toBe(true);
+    })
+})
+
+/*
+describe("Testiranje filter funkcije", function () {
+
+    it("treba da vrati samo parne brojeve", function () {
+        const rezultat = vratiParne([56, 26, 0, 77, 39, 346, 85, 55, 234, 5651, 5687, 588]);
+        let nadjenNeparni = false;
+        for (let i = 0; i < rezultat.length; i++) {
+            if (rezultat[i] % 2 !== 0) {
+                nadjenNeparni = true;
+                break;
+            }
+        }
+        expect(nadjenNeparni).toBe(false);
+    })
+})
+*/
+
